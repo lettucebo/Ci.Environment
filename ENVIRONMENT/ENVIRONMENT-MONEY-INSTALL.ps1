912ebd08afbf786a978c1f8a53a4ecf0b1812942
Money <abc12207@gmail.com>
2018-08-05 08:51:16 +0100

    Add ENVIRONMENT-MONEY-INSTALL.ps1

---
+++ ENVIRONMENT/ENVIRONMENT-MONEY-INSTALL.ps1
@@ -0,0 +1,65 @@
+Write-Output(Get-Date);
+
+## check admin right
+
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    Write-Warning "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
+    Break
+}
+
+## install chocolatey
+Write-Output("Start install chocolatey");
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Write-Output("Complete install chocolatey");
+
+## install Microsoft .NET Framework 4.7.1
+Write-Output("Start install Microsoft .NET Framework 4.7.1");
+choco install -y dotnet4.7.1
+Write-Output("Complete install chocolatey");
+
+## install .NET Core SDK
+Write-Output("Start install .NET Core SDK");
+choco install -y dotnetcore-sdk
+Write-Output("Complete install .NET Core SDK");
+
+## install VSCode
+Write-Output("Start install VSCode");
+choco install vscode -y --params "/NoDesktopIcon"
+Write-Output("Complete install VSCode");
+
+# choco install -y firefox-dev --pre 
+
+## install 7zip
+Write-Output("Start install 7zip");
+choco install -y 7zip.install
+Write-Output("Complete install 7zip");
+
+## install jre8
+choco install -y jdk8
+
+# choco install -y typora
+
+choco install -y sql-server-management-studio 
+
+choco install -y git.install  --params "/NoShellIntegration"
+
+choco install -y tortoisegit
+
+choco install -y rdcman
+
+choco install -y flashplayerplugin 
+
+choco install -y filezilla 
+
+choco install -y teamviewer 
+
+choco install -y potplayer 
+
+# choco install -y spotify 
+
+## File Explorer show hidden file and file extensions
+$explorerKey = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
+Set-ItemProperty $explorerKey Hidden 1
+Set-ItemProperty $explorerKey HideFileExt 0
+Stop-Process -processname explorer
