1aba25975e27e2ef58377286bca8d34b2f68ddf9
Money <abc12207@gmail.com>
2018-08-15 14:33:15 +0800

    Update ENVIRONMENT-MONEY-INSTALL.ps1

--- ENVIRONMENT/ENVIRONMENT-MONEY-INSTALL.ps1
+++ ENVIRONMENT/ENVIRONMENT-MONEY-INSTALL.ps1
@@ -78,6 +78,97 @@ choco install -y python
 $explorerKey = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
 Set-ItemProperty $explorerKey Hidden 1
 Set-ItemProperty $explorerKey HideFileExt 0
+
+## Remove Folders from This PC
+$regPath1 = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\'
+$regPath2 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\'
+
+$desktopItem = '{B4BFCC3A-DB2C-424C-B029-7FE99A87C641}'
+$documentsItem1 = '{A8CDFF1C-4878-43be-B5FD-F8091C1C60D0}'
+$documentsItem2 = '{d3162b92-9365-467a-956b-92703aca08af}'
+$downloadsItem1 = '{374DE290-123F-4565-9164-39C4925E467B}'
+$downloadsItem2 = '{088e3905-0323-4b02-9826-5d99428e115f}'
+$musicItem1 = '{1CF1260C-4DD0-4ebb-811F-33C572699FDE}'
+$musicItem2 = '{3dfdf296-dbec-4fb4-81d1-6a3438bcf4de}'
+$picturesItem1 = '{3ADD1653-EB32-4cb0-BBD7-DFA0ABB5ACCA}'
+$picturesItem2 = '{24ad3ad4-a569-4530-98e1-ab02f9417aa8}'
+$videosItem1 = '{A0953C92-50DC-43bf-BE83-3742FED03C9C}'
+$videosItem2 = '{f86fa3ab-70d2-4fc7-9c99-fcbf05467f3a}'
+$3dObjectsItem = '{0DB7E03F-FC29-4DC6-9020-FF41B59E513A}'
+
+# Remove Desktop From This PC
+Write-Host "Remove Desktop From This PC" -ForegroundColor Yellow
+If (Get-Item -Path $regPath1$desktopItem -ErrorAction SilentlyContinue) {
+    Remove-Item -Path $regPath1$desktopItem -Recurse
+    Remove-Item -Path $regPath2$desktopItem -Recurse
+} Else  {
+    Write-Host "Desktop key does not exist `n" -ForegroundColor Red
+}
+
+# Remove Documents From This PC
+Write-Host "Remove Documents From This PC" -ForegroundColor Yellow
+If (Get-Item -Path $regPath1$documentsItem1 -ErrorAction SilentlyContinue) {
+    Remove-Item -Path $regPath1$documentsItem1 -Recurse
+    Remove-Item -Path $regPath2$documentsItem1 -Recurse
+    Remove-Item -Path $regPath1$documentsItem2 -Recurse
+    Remove-Item -Path $regPath2$documentsItem2 -Recurse
+} Else  {
+    Write-Host "Documents key does not exist `n" -ForegroundColor Red
+}
+
+# Remove Downloads From This PC
+Write-Host "Remove Downloads From This PC" -ForegroundColor Yellow
+If (Get-Item -Path $regPath1$downloadsItem1 -ErrorAction SilentlyContinue) {
+    Remove-Item -Path $regPath1$downloadsItem1 -Recurse
+    Remove-Item -Path $regPath2$downloadsItem1 -Recurse
+    Remove-Item -Path $regPath1$downloadsItem2 -Recurse
+    Remove-Item -Path $regPath2$downloadsItem2 -Recurse
+} Else  {
+    Write-Host "Downloads key does not exist `n" -ForegroundColor Red
+}
+
+# Remove Music From This PC
+Write-Host "Remove Music From This PC" -ForegroundColor Yellow
+If (Get-Item -Path $regPath1$musicItem1 -ErrorAction SilentlyContinue) {
+    Remove-Item -Path $regPath1$musicItem1 -Recurse
+    Remove-Item -Path $regPath2$musicItem1 -Recurse
+    Remove-Item -Path $regPath1$musicItem2 -Recurse
+    Remove-Item -Path $regPath2$musicItem2 -Recurse
+} Else  {
+    Write-Host "Music key does not exist `n" -ForegroundColor Red
+}
+
+# Remove Pictures From This PC
+Write-Host "Remove Pictures From This PC" -ForegroundColor Yellow
+If (Get-Item -Path $regPath1$picturesItem1 -ErrorAction SilentlyContinue) {
+    Remove-Item -Path $regPath1$picturesItem1 -Recurse
+    Remove-Item -Path $regPath2$picturesItem1 -Recurse
+    Remove-Item -Path $regPath1$picturesItem2 -Recurse
+    Remove-Item -Path $regPath2$picturesItem2 -Recurse
+} Else  {
+    Write-Host "Pictures key does not exist `n" -ForegroundColor Red
+}
+
+# Remove Videos From This PC
+Write-Host "Remove Videos From This PC" -ForegroundColor Yellow
+If (Get-Item -Path $regPath1$videosItem1 -ErrorAction SilentlyContinue) {
+    Remove-Item -Path $regPath1$videosItem1 -Recurse
+    Remove-Item -Path $regPath2$videosItem1 -Recurse
+    Remove-Item -Path $regPath1$videosItem2 -Recurse
+    Remove-Item -Path $regPath2$videosItem2 -Recurse
+} Else  {
+    Write-Host "Videos key does not exist `n" -ForegroundColor Red
+}
+
+# Remove 3D Objects From This PC
+Write-Host "Remove 3DObjects From This PC" -ForegroundColor Yellow
+If (Get-Item -Path $regPath1$3dObjectsItem -ErrorAction SilentlyContinue) {
+    Remove-Item -Path $regPath1$3dObjectsItem -Recurse
+    Remove-Item -Path $regPath2$3dObjectsItem -Recurse
+} Else  {
+    Write-Host "3DObjects key does not exist `n" -ForegroundColor Red
+}
+
 Stop-Process -processname explorer
 
 ## Enable Microsoft-Windows-Subsystem-Linux
