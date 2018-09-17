5d0e42415d8e14a4d7b0dedd0c4811032f04c35e
Money <abc12207@gmail.com>
2018-09-17 18:07:09 +0800

    add tomcat

---
+++ ENVIRONMENT/ENVIRONMENT-WIN-SERVER-API-INSTALL.ps1
@@ -0,0 +1,231 @@
+## Notice need to set Set-ExecutionPolicy Unrestricted first
+
+Write-Output(Get-Date);
+
+Set-ExecutionPolicy -ExecutionPolicy UnRestricted
+
+## check admin right
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Warning "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
+    Break
+}
+
+## Set Windows 10 Feature
+Write-Host "Set Windows 10 Feature" -ForegroundColor Green
+# Unpin Quick Access Documents and Pictures
+$quickDocPath = "C:\Users\" + $env:UserName + "\Documents"
+$quickPicPath = "C:\Users\" + $env:UserName + "\Pictures"
+
+$QuickAccess = New-Object -ComObject shell.application 
+$TargetObject = $QuickAccess.Namespace("shell:::{679f85cb-0220-4080-b29b-5540cc05aab6}").Items() | where {$_.Path -eq "$quickDocPath"} 
+If ($TargetObject -eq $null) { 
+    Write-Warning "Documents Path is not pinned to Quick Access." 
+} 
+Else { 
+    $TargetObject.InvokeVerb("unpinfromhome") 
+} 
+
+$TargetObject = $QuickAccess.Namespace("shell:::{679f85cb-0220-4080-b29b-5540cc05aab6}").Items() | where {$_.Path -eq "$quickPicPath"} 
+If ($TargetObject -eq $null) { 
+    Write-Warning "Pictures Path is not pinned to Quick Access." 
+} 
+Else { 
+    $TargetObject.InvokeVerb("unpinfromhome") 
+}
+
+# Reference: https://gist.github.com/NickCraver/7ebf9efbfd0c3eab72e9
+# Change Explorer home screen back to "This PC"
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Type DWord -Value 1
+# Disable Quick Access: Recent Files
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer -Name ShowRecent -Type DWord -Value 0
+# Disable Quick Access: Frequent Folders
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer -Name ShowFrequent -Type DWord -Value 0
+# Disable P2P Update downlods outside of local network
+Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeliveryOptimization\Config -Name DODownloadMode -Type DWord -Value 1
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeliveryOptimization -Name SystemSettingsDownloadMode -Type DWord -Value 3
+
+## Disable IE Enhanced Security Configuration (ESC)
+$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+Stop-Process -Name Explorer
+Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+
+## install chocolatey
+Write-Host "Install Chocolatey and Packages" -ForegroundColor Green
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install -y dotnet4.7.2
+choco install -y vscode --params "/NoDesktopIcon"
+choco install -y googlechrome
+choco install -y 7zip.install
+choco install -y jdk8 -params "both=true"
+choco install -y cmdermini
+
+## Add Cmder Here
+Write-Host "Add Cmder Here" -ForegroundColor Green
+$cmderCmd = @'
+cmd.exe /C 
+C:\tools\cmdermini\cmder.exe /REGISTER ALL
+'@
+Invoke-Expression -Command:$cmderCmd
+
+## File Explorer show hidden file and file extensions
+Write-Host "File Explorer show hidden file and file extensions" -ForegroundColor Green
+$explorerKey = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
+Set-ItemProperty $explorerKey Hidden 1
+Set-ItemProperty $explorerKey HideFileExt 0
+
+## Remove Folders from This PC
+Write-Host "Remove Folders from This PC" -ForegroundColor Green
+$regPath1 = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\'
+$regPath2 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\'
+
+$desktopItem = '{B4BFCC3A-DB2C-424C-B029-7FE99A87C641}'
+$documentsItem1 = '{A8CDFF1C-4878-43be-B5FD-F8091C1C60D0}'
+$documentsItem2 = '{d3162b92-9365-467a-956b-92703aca08af}'
+$downloadsItem1 = '{374DE290-123F-4565-9164-39C4925E467B}'
+$downloadsItem2 = '{088e3905-0323-4b02-9826-5d99428e115f}'
+$musicItem1 = '{1CF1260C-4DD0-4ebb-811F-33C572699FDE}'
+$musicItem2 = '{3dfdf296-dbec-4fb4-81d1-6a3438bcf4de}'
+$picturesItem1 = '{3ADD1653-EB32-4cb0-BBD7-DFA0ABB5ACCA}'
+$picturesItem2 = '{24ad3ad4-a569-4530-98e1-ab02f9417aa8}'
+$videosItem1 = '{A0953C92-50DC-43bf-BE83-3742FED03C9C}'
+$videosItem2 = '{f86fa3ab-70d2-4fc7-9c99-fcbf05467f3a}'
+$3dObjectsItem = '{0DB7E03F-FC29-4DC6-9020-FF41B59E513A}'
+
+# Remove Desktop From This PC
+Write-Host "Remove Desktop From This PC" -ForegroundColor Yellow
+If (Get-Item -Path $regPath1$desktopItem -ErrorAction SilentlyContinue) {
+    Remove-Item -Path $regPath1$desktopItem -Recurse
+    Remove-Item -Path $regPath2$desktopItem -Recurse
+}
+Else {
+    Write-Warning "Desktop key does not exist `n"
+}
+
+# Remove Documents From This PC
+Write-Host "Remove Documents From This PC" -ForegroundColor Yellow
+If (Get-Item -Path $regPath1$documentsItem1 -ErrorAction SilentlyContinue) {
+    Remove-Item -Path $regPath1$documentsItem1 -Recurse
+    Remove-Item -Path $regPath2$documentsItem1 -Recurse
+    Remove-Item -Path $regPath1$documentsItem2 -Recurse
+    Remove-Item -Path $regPath2$documentsItem2 -Recurse
+}
+Else {
+    Write-Warning "Documents key does not exist `n"
+}
+
+# Remove Downloads From This PC
+Write-Host "Remove Downloads From This PC" -ForegroundColor Yellow
+If (Get-Item -Path $regPath1$downloadsItem1 -ErrorAction SilentlyContinue) {
+    Remove-Item -Path $regPath1$downloadsItem1 -Recurse
+    Remove-Item -Path $regPath2$downloadsItem1 -Recurse
+    Remove-Item -Path $regPath1$downloadsItem2 -Recurse
+    Remove-Item -Path $regPath2$downloadsItem2 -Recurse
+}
+Else {
+    Write-Warning "Downloads key does not exist `n"
+}
+
+# Remove Music From This PC
+Write-Host "Remove Music From This PC" -ForegroundColor Yellow
+If (Get-Item -Path $regPath1$musicItem1 -ErrorAction SilentlyContinue) {
+    Remove-Item -Path $regPath1$musicItem1 -Recurse
+    Remove-Item -Path $regPath2$musicItem1 -Recurse
+    Remove-Item -Path $regPath1$musicItem2 -Recurse
+    Remove-Item -Path $regPath2$musicItem2 -Recurse
+}
+Else {
+    Write-Warning "Music key does not exist `n"
+}
+
+# Remove Pictures From This PC
+Write-Host "Remove Pictures From This PC" -ForegroundColor Yellow
+If (Get-Item -Path $regPath1$picturesItem1 -ErrorAction SilentlyContinue) {
+    Remove-Item -Path $regPath1$picturesItem1 -Recurse
+    Remove-Item -Path $regPath2$picturesItem1 -Recurse
+    Remove-Item -Path $regPath1$picturesItem2 -Recurse
+    Remove-Item -Path $regPath2$picturesItem2 -Recurse
+}
+Else {
+    Write-Warning "Pictures key does not exist `n"
+}
+
+# Remove Videos From This PC
+Write-Host "Remove Videos From This PC" -ForegroundColor Yellow
+If (Get-Item -Path $regPath1$videosItem1 -ErrorAction SilentlyContinue) {
+    Remove-Item -Path $regPath1$videosItem1 -Recurse
+    Remove-Item -Path $regPath2$videosItem1 -Recurse
+    Remove-Item -Path $regPath1$videosItem2 -Recurse
+    Remove-Item -Path $regPath2$videosItem2 -Recurse
+}
+Else {
+    Write-Warning "Videos key does not exist `n"
+}
+
+# Remove 3D Objects From This PC
+Write-Host "Remove 3DObjects From This PC" -ForegroundColor Yellow
+If (Get-Item -Path $regPath1$3dObjectsItem -ErrorAction SilentlyContinue) {
+    Remove-Item -Path $regPath1$3dObjectsItem -Recurse
+    Remove-Item -Path $regPath2$3dObjectsItem -Recurse
+}
+Else {
+    Write-Warning "3DObjects key does not exist `n"
+}
+
+Stop-Process -processname explorer
+
+# Enable Telnet Client
+Write-Host "Enable Telnet Client" -ForegroundColor Green
+Enable-WindowsOptionalFeature -Online -FeatureName TelnetClient
+
+# Install VSCode Extensions
+Write-Host "Install VSCode Extensions" -ForegroundColor Green
+$codeExtensionCmd = @'
+cmd.exe /C 
+code --install-extension formulahendry.vscode-mysql
+code --install-extension ms-mssql.mssql
+code --install-extension formulahendry.auto-close-tag
+code --install-extension ms-azuretools.vscode-azureappservice
+code --install-extension vsciot-vscode.azure-iot-edge
+code --install-extension vsciot-vscode.azure-iot-toolkit
+code --install-extension coenraads.bracket-pair-colorizer
+code --install-extension ms-vscode.csharp
+code --install-extension peterjausovec.vscode-docker
+code --install-extension joelday.docthis
+code --install-extension sirtori.indenticator
+code --install-extension zhuangtongfa.material-theme
+code --install-extension ms-vscode.powershell
+code --install-extension esbenp.prettier-vscode
+code --install-extension humao.rest-client
+code --install-extension robinbentley.sass-indented
+code --install-extension wayou.vscode-todo-highlight
+code --install-extension octref.vetur
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension robertohuertasm.vscode-icons
+code --install-extension dotjoshjohnson.xml
+code --install-extension ritwickdey.liveserver
+'@
+Invoke-Expression -Command:$codeExtensionCmd
+
+## Install Developer Font
+Write-Host "Install Developer Font" -ForegroundColor Green
+$fontUrl = "https://onedrive.live.com/download?cid=9FBB0DE07F2BDB9D&resid=9FBB0DE07F2BDB9D%217415&authkey=AG0Y5D8cspzzmIM";
+$fontFile = "$PSScriptRoot\YaHei.ttf";
+Invoke-WebRequest -Uri $fontUrl -OutFile $fontFile
+
+$FONTS = 0x14
+$objShell = New-Object -ComObject Shell.Application
+$objFolder = $objShell.Namespace($FONTS)
+$objFolder.CopyHere($fontFile, 0x10)
+
+## Install Tomcat
+Write-Host "Install Tomcat" -ForegroundColor Green
+$tomcatUrl = "http://apache.stu.edu.tw/tomcat/tomcat-8/v8.5.34/bin/apache-tomcat-8.5.34.exe";
+$tomcatFile = "$PSScriptRoot\apache-tomcat-8.5.34.exe";
+Invoke-WebRequest -Uri $tomcatUrl -OutFile $tomcatFile
+& $tomcatFile `
+#    /S /D=D:\Tomcat
+
