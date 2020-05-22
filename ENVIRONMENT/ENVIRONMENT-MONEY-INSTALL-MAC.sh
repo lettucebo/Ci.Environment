57c1968e2dd446defc6a801f89a161d27696af23
Money <abc12207@gmail.com>
2020-05-23 03:35:10 +0800

    Update ENVIRONMENT-MONEY-INSTALL-MAC.sh

--- ENVIRONMENT/ENVIRONMENT-MONEY-INSTALL-MAC.sh
+++ ENVIRONMENT/ENVIRONMENT-MONEY-INSTALL-MAC.sh
@@ -8,6 +8,7 @@ brew tap caskroom/cask
 brew install node
 brew instakk nvm
 brew instakk git
+brew install azure-cli
 
 # Install cask software
 brew cask install google-chrome
@@ -23,7 +24,6 @@ brew cask install fork
 brew cask install tunnelblick
 brew cask install visual-studio
 brew cask install visual-studio-code
-brew cask install azure-cli
 brew cask install azure-data-studio   
 brew cask install filezilla
 brew cask install docker
@@ -31,6 +31,7 @@ brew cask install adobe-acrobat-reader
 brew cask install teamviewer
 brew cask install obs
 brew cask install scroll-reverser
+brew cask install onedrive
 
 # Install .NET Core SDK
 # https://dot.net/v1/dotnet-install.sh
