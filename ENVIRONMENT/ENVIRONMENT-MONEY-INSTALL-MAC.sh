94ab2401c8d67757f3720d7d67a12fbd197263a3
Money <abc12207@gmail.com>
2020-05-22 22:17:41 +0800

    Update ENVIRONMENT-MONEY-INSTALL-MAC.sh

--- ENVIRONMENT/ENVIRONMENT-MONEY-INSTALL-MAC.sh
+++ ENVIRONMENT/ENVIRONMENT-MONEY-INSTALL-MAC.sh
@@ -1,2 +1,20 @@
 # Install HomeBrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# Install cask
+brew tap caskroom/cask
+
+# Install software
+
+# Install cask software
+brew cask install google-chrome
+brew cask install microsoft-office
+brew cask install microsoft-teams
+brew cask install microsoft-azure-storage-explorer
+brew cask install microsoft-edge-beta
+brew cask install iina
+brew cask install spotify
+brew cask install firefox-developer-edition
+brew cask install git
+brew cask install snagit
+brew cask install fork
