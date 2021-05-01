57ac030dfb868b3143eb7ec8d8de269a7ee07eee
Money <abc12207@gmail.com>
2021-05-01 15:26:04 +0800

    Update ENVIRONMENT-MONEY-MS-INSTALL.ps1

--- ENVIRONMENT/ENVIRONMENT-MONEY-MS-INSTALL.ps1
+++ ENVIRONMENT/ENVIRONMENT-MONEY-MS-INSTALL.ps1
@@ -1,5 +1,5 @@
 ## Install chocolatey package
 choco install -y streamdeck
 choco install -y obs-studio
-choco install -y powerbi
+choco install -y powerbi --ignore-checksums
 choco install -y obs-ndi
