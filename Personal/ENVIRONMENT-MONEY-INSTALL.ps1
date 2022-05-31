4e6177c5ec2299d66cb3b790ef1b6d83feb2ce4d
Money <abc12207@gmail.com>
2022-05-31 22:59:08 +0800

    Update ENVIRONMENT-MONEY-INSTALL.ps1

--- Personal/ENVIRONMENT-MONEY-INSTALL.ps1
+++ Personal/ENVIRONMENT-MONEY-INSTALL.ps1
@@ -12,6 +12,9 @@ If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     Break
 }
 
+## Set traditional context menu
+reg.exe add “HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32” /f
+
 ## Set Windows 10 Feature
 Write-Host "Set Windows 10 Feature" -ForegroundColor Green
 # Unpin Quick Access Documents and Pictures
