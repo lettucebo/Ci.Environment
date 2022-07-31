4368e87cf875549aa8dd91f41caf42f147bc5adf
Money <abc12207@gmail.com>
2022-07-31 18:44:23 +0800

    Update 02.ENVIRONMENT-MONEY-INSTALL.ps1

--- Personal/02.ENVIRONMENT-MONEY-INSTALL.ps1
+++ Personal/02.ENVIRONMENT-MONEY-INSTALL.ps1
@@ -119,7 +119,7 @@ choco install -y microsoft-windows-terminal
 choco install -y terraform
 choco install -y python
 choco install -y gpg4win
-choco install -y snagit
+choco install -y snagit --ignorechecksum
 choco install -y robo3t
 choco install -y office365business
 choco install -y googlechrome
