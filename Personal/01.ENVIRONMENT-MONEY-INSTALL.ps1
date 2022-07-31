42741c8025a44b1549af26481465d54385b0eb0f
Money <abc12207@gmail.com>
2022-07-31 22:09:12 +0800

    Update 01.ENVIRONMENT-MONEY-INSTALL.ps1

--- Personal/01.ENVIRONMENT-MONEY-INSTALL.ps1
+++ Personal/01.ENVIRONMENT-MONEY-INSTALL.ps1
@@ -17,12 +17,6 @@ If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 Write-Host "Install PowerShell 7" -ForegroundColor Green
 # https://github.com/PowerShell/PowerShell/blob/master/tools/install-powershell.ps1-README.md
 iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI -Quiet"
-# # https://github.com/PowerShell/PowerShell/releases
-# $ps7Url = "https://github.com/PowerShell/PowerShell/releases/download/v7.2.5/PowerShell-7.2.5-win-x64.msi";
-# $ps7Msi = "$PSScriptRoot\PowerShell-7.2.5-win-x64.msi";
-# Invoke-WebRequest -Uri $ps7Url -OutFile $ps7Msi
-# # https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-windows?view=powershell-7.2#install-the-msi-package-from-the-command-line
-# msiexec.exe /package $ps7Msi /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1
 Write-Host "Install PowerShell 7 Complete" -ForegroundColor Green
 
 # TODO start with new PowerShell 7 windows and continue
