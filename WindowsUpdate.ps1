b9c3b7d6c5aed177bc069219e22004016f568853
Money Yu <abc12207@gmail.com>
2022-08-01 22:26:44 +0800

    remove unused file

--- WindowsUpdate.ps1
+++
@@ -1,5 +0,0 @@
-$upgradeUrl = "https://go.microsoft.com/fwlink/?LinkID=799445";
-$upgradeMsi = "$PSScriptRoot\Windows10Upgrade.exe";
-Invoke-WebRequest -Uri $upgradeUrl -OutFile $upgradeMsi
-
-Start-Process -FilePath $upgradeMsi -ArgumentList "/quietinstall /skipeula /auto upgrade"
