287ee47d6724250a995e3ccf952fc8965f42b13f
Money <abc12207@gmail.com>
2022-09-04 19:46:34 +0800

    Update 02.Setup01.ps1

--- Personal/ENVIRONMENT-MONEY-INSTALL/02.Setup01.ps1
+++ Personal/ENVIRONMENT-MONEY-INSTALL/02.Setup01.ps1
@@ -136,6 +136,7 @@ choco install -y microsoft-teams.install
 choco install -y sysinternals
 choco install -y openssl.light
 choco install -y nssm
+choco install -y autohotkey
 
 choco install -y dotnetcore-2.1-sdk
 choco install -y dotnetcore-2.2-sdk
