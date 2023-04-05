ae750b848d56ba93c94b2c7b034687ea3f7761b6
Money <abc12207@gmail.com>
2023-04-05 15:13:08 +0800

    Update 02.Setup01.ps1
    add Samsung Notes

--- Personal/ENVIRONMENT-MONEY-INSTALL/02.Setup01.ps1
+++ Personal/ENVIRONMENT-MONEY-INSTALL/02.Setup01.ps1
@@ -402,6 +402,8 @@ winget install XPDDVC6XTQQKMM --accept-package-agreements --accept-source-agreem
 winget install 9N4066W2R5Q4 --accept-package-agreements --accept-source-agreements
 # Bing Wallpaper
 winget install Microsoft.BingWallpaper --accept-package-agreements --accept-source-agreements
+# Samsung Notes
+winget install 9NBLGGH43VHV --accept-package-agreements --accept-source-agreements
 
 # Enable Telnet Client
 Write-Host "`n Enable Telnet Client" -ForegroundColor Green
