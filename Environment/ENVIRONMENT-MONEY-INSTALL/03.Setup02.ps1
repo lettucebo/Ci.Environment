3b33cf4d6fa21a6a7e6615ea5a9852f51810477c
Money Yu <abc12207@gmail.com>
2025-08-13 16:44:30 +0800

    Refactor setup script: improve warning messages, enhance nodejs installation logic, and clean up formatting

--- Environment/ENVIRONMENT-MONEY-INSTALL/03.Setup02.ps1
+++ Environment/ENVIRONMENT-MONEY-INSTALL/03.Setup02.ps1
@@ -5,36 +5,59 @@ Set-ExecutionPolicy RemoteSigned -Force
 
 ## check admin right
 If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    Write-Warning "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
-    Break
+  Write-Warning "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
+  Break
 }
 
 # Make sure have launched Visual Studio at least once before running this script.
 Write-Host "Please make sure you have launched Visual Studio at least once. Otherwise, installing extensions may fail." -ForegroundColor Yellow
 $vsConfirm = Read-Host "Have you already launched Visual Studio? (Y/N)"
 if ($vsConfirm -ne 'Y' -and $vsConfirm -ne 'y') {
-    Write-Warning "Please launch Visual Studio first, then re-run this setup script."
-    exit
+  Write-Warning "Please launch Visual Studio first, then re-run this setup script."
+  exit
 }
 
 ## Check Powershell version
-if($PSversionTable.PsVersion.Major -lt 7){
-    Write-Error "Please use Powershell 7 to execute this script!"
-    Break
+if ($PSversionTable.PsVersion.Major -lt 7) {
+  Write-Error "Please use Powershell 7 to execute this script!"
+  Break
 }
 
-# Install nodejs using nvm
-Write-Host "`n Install nodejs using nvm" -ForegroundColor Green
-$nvmCmd = @'
-cmd.exe /C 
-nvm install 20.17.0
-nvm install 22.6.0
-nvm use 22.6.0
-'@
-Invoke-Expression -Command:$nvmCmd
+## Install nodejs using nvm (auto select LTS and Current)
+Write-Host "`n Install nodejs using nvm (auto select LTS and Current)" -ForegroundColor Green
 
-# Install GPG agent auto start service
-## https://stackoverflow.com/a/51407128/1799047
+$nvmList = cmd.exe /c "nvm list available"
+
+# Find the first line containing version numbers (skip table header)
+$versionLine = ($nvmList | Where-Object { $_ -match '^\s*\|\s*\d' } | Select-Object -First 1)
+
+if ($versionLine) {
+  # Split by | and get CURRENT and LTS columns
+  $columns = $versionLine -split '\|'
+  $currentVersion = $columns[1].Trim()
+  $ltsVersion = $columns[2].Trim()
+}
+else {
+  $currentVersion = ""
+  $ltsVersion = ""
+}
+
+Write-Host "Detected LTS version: $ltsVersion"
+Write-Host "Detected Current version: $currentVersion"
+
+if ($ltsVersion) {
+  cmd.exe /c "nvm install $ltsVersion"
+}
+if ($currentVersion -and $currentVersion -ne $ltsVersion) {
+  cmd.exe /c "nvm install $currentVersion"
+  cmd.exe /c "nvm use $currentVersion"
+}
+elseif ($ltsVersion) {
+  cmd.exe /c "nvm use $ltsVersion"
+}
+
+## Install GPG agent auto start service
+# https://stackoverflow.com/a/51407128/1799047
 nssm install GpgAgentService "C:\Program Files (x86)\GnuPG\bin\gpg-agent.exe"
 nssm set GpgAgentService AppDirectory "C:\Program Files (x86)\GnuPG\bin"
 nssm set GpgAgentService AppParameters "--launch gpg-agent"
@@ -44,7 +67,6 @@ nssm set GpgAgentService Description "Auto start gpg-agent"
 Write-Host "`n Install Visual Studio Exntension" -ForegroundColor Green
 $vsixInstallScript = "$PSScriptRoot\install-vsix.ps1";
 Invoke-WebRequest -Uri "https://gist.githubusercontent.com/lettucebo/1c791b21bf56f467254bc85fd70631f4/raw/5dc3ff85b38058208d203383c54d8b7818365566/install-vsix.ps1" -OutFile $vsixInstallScript
-# & $vsixInstallScript -PackageName "ErlandR.ReAttach"
 & $vsixInstallScript -PackageName "MadsKristensen.FileIcons"
 & $vsixInstallScript -PackageName "MadsKristensen.ZenCoding"
 & $vsixInstallScript -PackageName "MadsKristensen.EditorConfig"
@@ -81,22 +103,22 @@ netsh int ipv4 add excludedportrange protocol=tcp numberofports=1 startport=8888
 
 ## Run basic docker
 docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=P@ssw0rd" `
-   -p 1433:1433 --name mssql2022 --hostname mssql2022 `
-   -d `
-   --restart unless-stopped `
-   mcr.microsoft.com/mssql/server:2022-latest
+  -p 1433:1433 --name mssql2022 --hostname mssql2022 `
+  -d `
+  --restart unless-stopped `
+  mcr.microsoft.com/mssql/server:2022-latest
  
- docker run --name redis `
- -p 6379:6379 `
- -d `
- --restart unless-stopped `
- redis
+docker run --name redis `
+  -p 6379:6379 `
+  -d `
+  --restart unless-stopped `
+  redis
  
- docker run -e "POSTGRES_PASSWORD=P@ssw0rd" `
-   -p 5432:5432 --name postgres --hostname postgres `
-   -d `
-   --restart unless-stopped `
-   postgres
+docker run -e "POSTGRES_PASSWORD=P@ssw0rd" `
+  -p 5432:5432 --name postgres --hostname postgres `
+  -d `
+  --restart unless-stopped `
+  postgres
  
 ## Complete
 Write-Host -NoNewLine "`n Environment config complete, Press any key to continue...";
